a550b2fe28ae4e6a71a652d790bc3d50ee71e07d
wejden <wejdenchneti7@gmail.om>
2020-04-05 11:39:18 +0100

    error coriges

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,8 +5,7 @@ import { FormsModule} from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule }from "@angular/common/http";
-import { IgxRadioModule } from 'igniteui-angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FusionChartsModule } from 'angular-fusioncharts';
@@ -115,8 +114,7 @@ FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme)
     NgxPaginationModule,
     RxReactiveFormsModule,
     Ng2SearchPipeModule,
-    IgxRadioModule,
-    BrowserAnimationsModule
+   
  
   
   ],
